c58c789a6812f3478473396120e6b673b3e8b37e
noahAtYu <noahpotash@gmail.com>
2019-03-19 16:25:09 -0400

    added extract append

--- Antlr_demo/src/main/java/TestGoobScraperVisitor.java
+++ Antlr_demo/src/main/java/TestGoobScraperVisitor.java
@@ -14,6 +14,25 @@ public class TestGoobScraperVisitor<T> extends GoobScraperBaseVisitor {
     }
 
     @Override
+    public Variable visitGetURL(GoobScraperParser.GetURLContext ctx) {
+        //https://en.wikipedia.org/wiki/Oversampling_and_undersampling_in_data_analysis
+        URLConnection connection = null;
+        try {
+            String url = ctx.word().getText().replace("\"","");
+            connection = new URL(url).openConnection();
+            InputStream response = connection.getInputStream();
+            Scanner scanner = new Scanner(response);
+            String responseBody = scanner.useDelimiter("\\A").next();
+            System.out.println(responseBody);
+            return Variable.variableFactory(responseBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Override
     public Void visitWordID(GoobScraperParser.WordIDContext ctx) {
         String id = ctx.ID().getText();
         return null;
@@ -48,7 +67,12 @@ public class TestGoobScraperVisitor<T> extends GoobScraperBaseVisitor {
             file = ctx.getChild(2).getText();
         }
         try {
-            FileWriter writer = new FileWriter(file.replace("\"",""));
+            FileWriter writer = new FileWriter(file.replace("\"",""),false);
+            //test example:
+            writer.append("ID");writer.append(',');writer.append("name");writer.append('\n');
+
+            writer.flush();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -57,27 +81,27 @@ public class TestGoobScraperVisitor<T> extends GoobScraperBaseVisitor {
 
     @Override
     public Void visitExtractAppend(GoobScraperParser.ExtractAppendContext ctx) {
-        //String string = ctx
-        return null;
-    }
-
-    @Override
-    public Variable visitGetURL(GoobScraperParser.GetURLContext ctx) {
-        //https://en.wikipedia.org/wiki/Oversampling_and_undersampling_in_data_analysis
-        URLConnection connection = null;
+        String file = "",var = "";
+        int wordNum = ctx.word().size();
+        if(wordNum == 1){
+            file = ctx.getChild(1).getText();
+        }else if(wordNum == 2){
+            var = ctx.getChild(1).getText();
+            file = ctx.getChild(2).getText();
+        }
         try {
-            String url = ctx.word().getText().replace("\"","");
-            connection = new URL(url).openConnection();
-            InputStream response = connection.getInputStream();
-            Scanner scanner = new Scanner(response);
-            String responseBody = scanner.useDelimiter("\\A").next();
-            System.out.println(responseBody);
-            return Variable.variableFactory(responseBody);
+            FileWriter writer = new FileWriter(file.replace("\"",""),true);
+            //test example:
+            writer.append("1");writer.append(',');writer.append("bob");writer.append('\n');
+
+            writer.flush();
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
