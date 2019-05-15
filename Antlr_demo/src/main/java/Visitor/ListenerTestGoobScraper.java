1a1334dfe2a25d899b48b171367c9d3399d351db
noahAtYu <noahpotash@gmail.com>
2019-05-15 15:21:39 -0400

    added python to listener

--- Antlr_demo/src/main/java/Visitor/ListenerTestGoobScraper.java
+++ Antlr_demo/src/main/java/Visitor/ListenerTestGoobScraper.java
@@ -27,60 +27,124 @@ public class ListenerTestGoobScraper extends GoobScraperBaseListener {
         System.out.println("In exitGetURL");
         System.out.println(ctx.word().getText());
         URLConnection connection = null;
-        try {
-            Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("testGoober.js"), "utf-8"));
-            String url = ctx.word().getText().replace("\"", "");
-            String jsCode = "const getURL = function (url) {\n" +
-                    "    const request = require('request');\n" +
-                    "    url = url.replace(/['\";]+/g, '');\n" +
-                    "    console.log(url);\n" +
-                    "    request(url, (err, res, body) => {\n" +
-                    "        if (err) {\n" +
-                    "            return console.log(err);\n" +
-                    "        }\n" +
-                    "        console.log(body);\n" +
-                    "    });\n" +
-                    "};\n" +
-                    "getURL(" + "\"" + url + "\");";
+        String url = ctx.word().getText().replace("\"", "");
 
-            String js2 = "    request(url, (err, res, body) => {\n" +
-                    "        if (err) {\n" +
-                    "            return console.log(err);\n" +
-                    "        }\n" +
-                    "        console.log(body);\n" +
-                    "    });\n";
-            writer.write(jsCode);
-            writer.close();
-            /*connection = new URL(url).openConnection();
-            InputStream response = connection.getInputStream();
-            Scanner scanner = new Scanner(response);
-            String responseBody = scanner.useDelimiter("\\A").next();
-            System.out.println(responseBody);
-            lastVar = Variable.variableFactory(url, responseBody);
-            varMem.put(lastVar.getName(), lastVar);
-            lastVar.addStep("/get url " + ctx.word().getText());
-            System.out.println("Visitor.Variable reference name: " + lastVar.getName());
-            */
+        String code =
+                "import requests\n" +
+                "from bs4 import BeautifulSoup\n" +
+                "memory = {} # map for memory\n" +
+                "varNumStrt = 0\n" +
+                "global lastVar\n" +
+                "lastVar = -1\n" +
+                "def getURL(url):\n" +
+                "    global lastVar\n" +
+                "    lastVar += 1\n" +
+                "    response = requests.get(url)\n" +
+                "    memory[lastVar] = response.text\n" +
+                "    print(\"Variable reference name: \" + str(lastVar))\n" +
+                "    return response\n" +
+                "\n" +
+                "res = getURL(\""+ url + "\")";
+
+        try(FileWriter fw = new FileWriter("testGoober.py", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw)){
+            out.print(code);
+            //fw.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            //exception handling
         }
     }
 
-    public static void main(String[] args){
-        while(true){
-            //System.out.print("~$: ");
-            try {
-                Scanner sc = new Scanner(System.in);
-                //String fileName = sc.next();
-                String fileName = "./src/main/java/Visitor/input.txt";
-                BufferedReader br = new BufferedReader(new FileReader(fileName));
-                String inputLine = br.readLine();
-                parseAndRunLine(inputLine);
-            } catch (IOException e) {
-                e.printStackTrace();
+    //ex. "/get table VAR" or "/get table";
+    @Override
+    public void exitGetTable(GoobScraperParser.GetTableContext ctx) {
+        String pyLine = "";
+        System.out.println("In exitGetTable");
+        int wordNum = ctx.children.size() - 2;
+        if(wordNum == 1){//extract new "asdf.csv";
+            pyLine = "    html = memory[lastVar]\n";
 
-            }
+        }else if(wordNum == 2){//extract new 0 "asdf.csv";
+            String varNum = ctx.getChild(2).getText();//get from memory
+            pyLine = "    html = memory[" + varNum + "]\n";
+
         }
+        String code =
+                    "def getTable():\n" +
+                    "    try:\n" +
+                    "        data = []\n" +
+                    "        var= lastVar\n" +
+                             pyLine +
+                    "        soup = BeautifulSoup(html,\"lxml\")\n" +
+                    "        table = soup.find( \"table\")\n" +
+                    "        rows = table.find_all('tr')\n" +
+                    "        for row in rows:\n" +
+                    "            cols = row.findAll('td')\n" +
+                    "            if cols != []:\n" +
+                    "                cols = [ele.text.strip() for ele in cols]\n" +
+                    "                data.append([ele for ele in cols if ele != []])  # Get rid of empty values\n" +
+                    "        memory[lastVar] = data\n" +
+                    "    except ValueError:\n" +
+                    "        \"No variable there\"\n" +
+                    "tables = getTable()";
+        try(FileWriter fw = new FileWriter("testGoober.py", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw)){
+            out.print(code);
+            //fw.close();
+        } catch (IOException e) {
+            //exception handling
+        }
+
+    }
+
+    @Override
+    public void exitExtractStatment(GoobScraperParser.ExtractStatmentContext ctx) {
+        String file = "";
+        String pyStatment = "";
+        int wordNum = ctx.word().size();
+        if(wordNum == 1){//extract new "asdf.csv";
+            file = ctx.getChild(2).getText();
+            pyStatment = "    lister = memory[lastVar]\n";
+
+        }else if(wordNum == 2){//extract new 0 "asdf.csv";
+            String varNum = ctx.getChild(2).getText();//get from memory
+            file = ctx.getChild(3).getText();
+            pyStatment = "    lister = memory[" + varNum + "]\n";
+
+        }
+        System.out.println();
+        String code = "def extractStat():\n" +
+                "    import pandas as pd\n" +
+                     pyStatment +
+                "    my_df = pd.DataFrame(lister)\n" +
+                "    my_df.to_csv(" + "\"" + file + "\"" + ", index=False, header=False)\n" +
+                "extractStat()";
+
+        try(FileWriter fw = new FileWriter("testGoober.py", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw)){
+            out.print(code);
+            //fw.close();
+        } catch (IOException e) {
+            //exception handling
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "./src/main/java/Visitor/input.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+            while (line != null) {
+                parseAndRunLine(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static void parseAndRunLine(String inputLine) {
